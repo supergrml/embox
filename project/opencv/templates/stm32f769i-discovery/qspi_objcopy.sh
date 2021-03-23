f88f68c78300427da9e3d5d7dc59735dcecbb1c9
Alex Kalmuk <alexkalmuk@gmail.com>
2021-03-23 18:14:51 +0300

    upd project/opencv/templates/stm32f769i-discovery/

--- project/opencv/templates/stm32f769i-discovery/qspi_objcopy.sh
+++ project/opencv/templates/stm32f769i-discovery/qspi_objcopy.sh
@@ -1,3 +1,3 @@
-#!/bin/sh
+#!/bin/sh -x
 
-arm-none-eabi-objcopy -O binary build/base/bin/embox build/base/bin/qspi.bin --only-section=.text --only-section=.rodata --only-section='.ARM.ex*' --only-section='.data'
+arm-none-eabi-objcopy -O binary build/base/bin/embox build/base/bin/cv.bin --only-section=.text --only-section=.rodata --only-section='.ARM.ex*' --only-section='.data'
